3b91b0d6f8916861246f09a02ec4afc9c8f93750
moussa doumbia <moussa.doumbia9219@gmail.com>
2022-11-23 08:27:33 +0100

    resolve issue with kyc creation on page startup

--- src/app/views/pages/auth/login/login.component.ts
+++ src/app/views/pages/auth/login/login.component.ts
@@ -21,10 +21,10 @@ export class LoginComponent implements OnInit {
   async onLoggedin(e: Event) {
     e.preventDefault();
     await localStorage.setItem('isLoggedin', 'true');
-    // await this.onboardingDomainService.createUserKyc();
-    if (localStorage.getItem('isLoggedin')) {
-      await this.router.navigate([this.returnUrl]);
-    }
+    const kyc = await this.onboardingDomainService.createUserKyc();
+    // if (localStorage.getItem('isLoggedin') && kyc['oid']) {
+    //   await this.router.navigate([this.returnUrl]);
+    // }
   }
 
 }
